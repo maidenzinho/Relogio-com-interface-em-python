8db027d889fffa8f88484b7e316ed882637ae35c
Felipe Fernandes <ff8778732@gmail.com>
2025-05-11 20:38:25 -0300

    1

---
+++ relogio.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from datetime import strftime
+
+def update_time():
+    current_time = strftime('%H:%M:%S %p')
+    label.config(text=current_time)
+    label.after(1000, update_time)
+
+root = tk.Tk()
+root.title('Relógio')
+
+label = tk.Label(root, font=('calibri', 40, 'bold'), background='blue', foreground='white')
+label.pack(anchor='center')
+
+update_time()
+
+root.mainloop()
